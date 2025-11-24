d70adccc6f5cb46a347965c59772c7cb45ecf68e
LuizFeipe <luiz.alves1@estudante.ifgoiano.edu.br>
2025-11-24 08:05:37 -0300

    Update

--- src/App.java
+++ src/App.java
@@ -4,7 +4,6 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class App {
-    //Update
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String raiz = "Aplicativo/";
